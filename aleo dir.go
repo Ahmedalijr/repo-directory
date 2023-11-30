f8425e13c103f6d66c6b18483fe6c5c59450b427
Ahmed Alijir <152516894+Ahmedalijr@users.noreply.github.com>
2023-11-30 18:15:09 +0200

    Create aleo dir.go

---
+++ aleo dir.go
@@ -0,0 +1,9 @@
+func (s *KeeperTestHelper) SetupValidator(bondStatus stakingtypes.BondStatus) sdk.ValAddress {
+	valPub := secp256k1.GenPrivKey().PubKey()
+	valAddr := sdk.ValAddress(valPub.Address())
+	bondDenom := s.App.StakingKeeper.GetParams(s.Ctx).BondDenom
+	selfBond := sdk.NewCoins(sdk.Coin{Amount: sdk.NewInt(100), Denom: bondDenom})
+
+	s.FundAcc(sdk.AccAddress(valAddr), selfBond)
+
+	val, found := s.App.StakingKeeper.GetValidator(s.Ctx, valAddr)
